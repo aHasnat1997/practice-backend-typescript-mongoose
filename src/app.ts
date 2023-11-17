ad09bfd5e65f45edecad8df14cd174df3723fe89
aHasnat1997 <hasnat56703@gmail.com>
2023-11-17 23:42:06 +0600

    view add

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
+import path from "path";
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello 🖕 World');
+    // res.send('Hello 🖕 World');
+    res.sendFile(path.join(__dirname, '../view/index.html'));
 })
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
+import mongoose from "mongoose";
 import app from "./app"
 import config from "./config";
 
-async function main() {
+(async function main() {
     try {
+        await mongoose.connect(config.database_url as string);
+
         app.listen(config.port, () => {
             console.log(`Server on 🔥 at ${config.port}`);
         });
     } catch (error) {
         console.log(error);
     }
-}
-main();
+})();
